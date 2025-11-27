b907ea7a82606e53f11520085dff7b8dd4b554e4
KIKW12 <a01711235@tec.mx>
2025-11-27 12:33:13 -0600

    Initial Commit

---
+++ examenMoviles-A01711235/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  examenMoviles-A01711235
+//
+//  Created by Enrique Ayala on 27/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ examenMoviles-A01711235/examenMoviles_A01711235App.swift
@@ -0,0 +1,17 @@
+//
+//  examenMoviles_A01711235App.swift
+//  examenMoviles-A01711235
+//
+//  Created by Enrique Ayala on 27/11/25.
+//
+
+import SwiftUI
+
+@main
+struct examenMoviles_A01711235App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
